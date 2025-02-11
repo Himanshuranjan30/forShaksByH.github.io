6dfed0f83b6cc81a7f8a99e9b977e4f6db443cde
Himanshu Ranjan <himanshu.ranjan@gohighlevel.com>
2025-02-11 22:21:50 +0530

    init: ss-vl

--- CONFIG.js
+++ CONFIG.js
@@ -1,5 +1,5 @@
 const CONFIGDATA = {
-  titleHeader: "Hey you, I have something to say. </br> Will you be my valentine?",
+  titleHeader: " </br> Will you be my valentine?",
   descriptionHeader:
     "If you dont respond, I'll take that as a YES! 💕",
   buttonYes: "Yes, I will!",
